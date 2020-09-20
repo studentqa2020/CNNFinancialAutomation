f332e596195d6e262454a53e494a28436f1263c0
studentqa2020 <sarow@LAPTOP-5EBHP9EP.home>
2020-09-20 13:11:38 -0400

    initial commit

---
+++ CNN_Financial_Automation/src/test/java/com/functional/testing/TestExecution.java
@@ -0,0 +1,37 @@
+package com.functional.testing;
+
+import org.testng.annotations.Test;
+
+public class TestExecution {
+
+	@Test
+	public void test01() {
+		//code	
+
+	}
+
+	@Test
+	public void test02() {
+
+		// code
+	}
+
+	@Test
+	public void test03() {
+
+		// code
+	}
+
+	@Test
+	public void test04() {
+
+		// code
+	}
+
+	@Test
+	public void test05() {
+
+		// code
+	}
+
+}
